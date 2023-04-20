0dd6ba2919d051c647cb91341763410cfb72b1e7
Yeonseo-Jo <jys073190@gmail.com>
2023-04-20 13:45:39 +0900

    rfc: 불필요한 코드 제거, 코드 설명 주석 추가

--- week2/assign1(myShop2)/script.js
+++ week2/assign1(myShop2)/script.js
@@ -1,12 +1,12 @@
 import ITEM_DATA from "./itemData.js";
 
-//확인용
 window.onload = function () {
   showCard(ITEM_DATA);
   handleCategory();
   onClickModal();
 };
 
+/***카드 로드 관련 함수 ***/
 // 카드 보여주기 - 일단 태그 두개로 고정 ..
 const showCard = (itemDatas) => {
   itemDatas.forEach((item) => {
@@ -43,15 +43,19 @@ const showCard = (itemDatas) => {
   });
 };
 
-//nav 카테고리 선택 구현
+/*** 카테고리 태그 관련 함수 ***/
+/*** global 변수 ***/
+// 선택 된 카테고리들의 id를 담는 리스트
 let selectedCateg = [];
+// 카테고리들을 담는 리스트
 const categories = [...document.querySelectorAll(".nav__category > input")];
-const selectAll = document.getElementById("ALL");
 
+// 카테고리 태그 관련 함수들을 최종 핸들링 해주는 함수
 const handleCategory = () => {
   categories.forEach((category) => {
     category.addEventListener("click", (e) => {
       let targetCateg = e.currentTarget;
+      // 태그가 중복으로 쌓이지 않도록 태그 영역을 초기화 해 줌
       initTags();
       handleCheck(targetCateg);
       showTags(selectedCateg);
@@ -60,19 +64,18 @@ const handleCategory = () => {
   });
 };
 
+// 체크박스의 선택 상태 관리 함수
 const handleCheck = (t) => {
   const currCateg = t.id;
   if (currCateg == "ALL") {
     // ALL 클릭시 전체 클릭, ALL 취소시 전체 취소
     categories.forEach((category) => {
-      category.checked = selectAll.checked;
-      handleSelectList();
+      category.checked = document.getElementById("ALL").checked;
     });
   } else {
     document.getElementById(currCateg).checked;
-    handleSelectList();
   }
-  //   handleSelectList();
+  handleSelectList();
 };
 
 //현재 선택된 카테고리 리스트를 관리하는 함수
@@ -94,11 +97,12 @@ const handleSelectList = () => {
     }
   });
 
+  // 리스트에 요소가 중복되지 않도록 set으로 중복 제거
   selectedCateg = [...new Set(selectedCateg)];
 };
 
+// 선택된 카테고리의 카테고리 태그를 나타내는 함수
 const showTags = (selectedCateg) => {
-  console.log(selectedCateg);
   selectedCateg.forEach((label) => {
     const tags = document.querySelector(".category-tags");
     const tag = document.createElement("div");
@@ -111,11 +115,11 @@ const showTags = (selectedCateg) => {
       `;
     tags.appendChild(tag);
 
+    // 카테고리 태그의 x버튼 삭제 구현
     const delBtn = tag.querySelector("i");
 
     delBtn.addEventListener("click", (e) => {
-      console.log(e.currentTarget);
-      // X 버튼으로 태그 삭제
+      // X 버튼을 누르면 태그가 삭제된다
       delCateg(e.currentTarget);
     });
   });
@@ -129,16 +133,18 @@ const initTags = () => {
   }
 };
 
-//카테고리 태그 삭제
+//카테고리 태그 삭제 함수
 const delCateg = (t) => {
   const tagLabel = t.parentNode.id;
   if (tagLabel === "ALL") {
     selectedCateg.forEach((tag) => {
       document.getElementById(tag.id).checked = false;
+      // ALL을 삭제하면 전체 태그 삭제, 선택된 태그도 초기화
       initTags();
       selectedCateg = [];
     });
   } else {
+    // 특정 태그를 삭제하면 해당 되는 카테고리 태그를 없애고, 선택된 태그 리스트에서도 빼 줌
     document.getElementById(tagLabel).checked = false;
     handleSelectList();
     t.parentNode.remove();
@@ -146,7 +152,7 @@ const delCateg = (t) => {
   filterCards();
 };
 
-// 카테고리에 맞게 카드 로드
+// 카테고리에 맞게 카드를 필터링 해서 보여주는 함수
 const filterCards = () => {
   const cards = [...document.getElementsByClassName("cards__cardContent")];
   const categIds = [];
@@ -160,15 +166,18 @@ const filterCards = () => {
     });
   } else {
     cards.forEach((card) => {
+      // 선택된 카테고리 태그가 ALL이면 모든 카드를 보여주고, 특정 카테고리 태그이면 해당되는 카테고리를 가진 카드만 보여준다.
       categIds.includes("ALL") || categIds.includes(card.dataset.category)
         ? (card.style.display = "flex")
-        : (card.style.display = "none");
+        : // 선택된 카테고리 태그에 없는 카테고리를 가진 카드는 숨겨준다.
+          (card.style.display = "none");
     });
   }
 };
 
-// 해시태그 모달
+/*** 해시태그 모달 구현 함수***/
 const onClickModal = () => {
+  // 해시태그 모달을 보여줄수 있는 + 버튼들의 리스트
   const targetBtns = [...document.getElementsByClassName("cards__tagBtn")];
 
   targetBtns.forEach((openBtn) => {
@@ -176,17 +185,15 @@ const onClickModal = () => {
     const targetModal = targetCardTag.querySelector(".cards__tagModal");
     const delBtn = targetModal.querySelector(".delModal");
 
+    // 해시태그 모달을 보여주는 이벤트
     openBtn.addEventListener("click", () => {
       console.log(targetModal);
       targetModal.style.display = "flex";
     });
 
+    // 해시태그 모달을 닫아주는 이벤트
     delBtn.addEventListener("click", () => {
       targetModal.style.display = "none";
     });
   });
 };
-
-// //함수 실행
-// showCard(ITEM_DATA);
-// handleCategory();
