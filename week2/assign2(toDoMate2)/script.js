f97baa5c0a06b7730c71dd7fceb9cb35a0d24746
Yeonseo-Jo <jys073190@gmail.com>
2023-04-21 01:07:02 +0900

    feat:카테고리별 할 일 추가 구현

--- week2/assign2(toDoMate2)/script.js
+++ week2/assign2(toDoMate2)/script.js
@@ -3,8 +3,9 @@ import TODO_DATA from "./todoData.js";
 window.onload = function () {
   showTodoList(TODO_DATA);
   initCnt();
-  const cnt = onClickDone();
-  // console.log(cnt);
+  onClickDone();
+  handleModal();
+  closeModal();
 };
 
 //투두리스트 영역을 로드하는 함수 (Template 이용!)
@@ -21,6 +22,7 @@ const showTodoList = (todoDatas) => {
     newTodoHtml = newTodoHtml
       .replace("{color}", data.color)
       .replace("{category}", data.category)
+      .replace("{categoryName}", data.category)
       .replace("{todoContent}", todoContentHtml);
     htmlContent.innerHTML = newTodoHtml;
     todoSection.appendChild(htmlContent.content);
@@ -44,12 +46,23 @@ const showTodoContent = (todos) => {
   return todoContentHtml;
 };
 
+//!!
+let cntDone;
+// // let todoLists;
+// const cntLists = () => {
+//   const currTodoLists = [
+//     ...document.getElementsByClassName("todolist__content"),
+//   ];
+//   todoLists = currTodoLists;
+// };
 // 할일 목록의 완료 상태를 관리하는 이벤트 함수
+
 const onClickDone = () => {
   const todoLists = [...document.getElementsByClassName("todolist__content")];
+  console.log(todoLists);
   const targetDay = document.querySelector(".highlight");
   let remainNum = targetDay.querySelector(".calender__remainNum");
-  let cntDone = todoLists.length;
+  cntDone = todoLists.length;
 
   todoLists.forEach((targetLi) => {
     targetLi.addEventListener("click", () => {
@@ -69,10 +82,63 @@ const onClickDone = () => {
   return cntDone;
 };
 
+// 남은 할 일 개수 초기화 함수
 const initCnt = () => {
   const todoLists = [...document.getElementsByClassName("todolist__content")];
   const targetDay = document.querySelector(".highlight");
   let remainNum = targetDay.querySelector(".calender__remainNum");
   let cntDone = todoLists.length;
   remainNum.innerHTML = cntDone;
+};
+
+//모달 열고 닫기
+let targetCateg;
+const handleModal = () => {
+  const addBtns = [...document.getElementsByClassName("todolist__addBtn")];
+  const targetModal = document.querySelector(".addTodo__Modal");
+  const targetForm = targetModal.querySelector(".modal__form");
+  const targetInput = targetForm.querySelector("input");
+
+  addBtns.forEach((targetBtn) => {
+    targetBtn.addEventListener("click", () => {
+      targetInput.value = null;
+      targetModal.style.display = "flex";
+      targetInput.focus();
+      targetCateg = targetBtn.parentNode.querySelector("h2").innerText;
+    });
+  });
+};
+
+const closeModal = () => {
+  const closeBtn = document.querySelector(".modal__submitBtn");
+  const targetModal = document.querySelector(".addTodo__Modal");
+
+  closeBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    handleForm(targetCateg);
+    targetModal.style.display = "none";
+    return false;
+  });
+};
+
+// 폼 정보 받아와서 새 할 일 리스트 만들기
+const handleForm = (category) => {
+  console.log("*****", category, "****");
+  const targetModal = document.querySelector(".addTodo__Modal");
+  const targetForm = targetModal.querySelector(".modal__form");
+  const targetInput = targetForm.querySelector("input");
+
+  console.log(targetInput.value);
+  console.log(category);
+  let currCateg = document.getElementById(category);
+  let newLi = document.createElement("li");
+  newLi.classList.add("todolist__content");
+  newLi.setAttribute("data-checked", false);
+  newLi.innerHTML = `
+    <i class="fa-solid fa-heart"></i>
+    <p class="todolist__detail">${targetInput.value}</p>
+    `;
+
+  currCateg.appendChild(newLi);
+  initCnt();
 };
