906859ade1f0ed6fda31db54aa41e255362658dc
Yeonseo-Jo <jys073190@gmail.com>
2023-05-02 23:35:02 +0900

    feat: 이미지 데이터 파일 추가

---
+++ week3/card_game/src/assets/datas/ANKO_DATA.js
@@ -0,0 +1,60 @@
+const ANKO_DATA = [
+  {
+    id: 1,
+    alt: "하트 앙꼬",
+    imgSrc:
+      "https://item.kakaocdn.net/do/2e89b42e05648ac85ee751c98ccc16d8f43ad912ad8dd55b04db6a64cddaf76d",
+  },
+  {
+    id: 2,
+    alt: "긍정회로 앙꼬",
+    imgSrc:
+      "https://item.kakaocdn.net/do/3ad50f2fc4efa7d7d48a5d8b914f8ba7f43ad912ad8dd55b04db6a64cddaf76d",
+  },
+  {
+    id: 3,
+    alt: "놀고먹는 앙꼬",
+    imgSrc:
+      "https://item.kakaocdn.net/do/bcf4ab7ffeca0d362f46e6a3f8c59ab0f43ad912ad8dd55b04db6a64cddaf76d",
+  },
+  {
+    id: 4,
+    alt: "루돌프 앙꼬",
+    imgSrc:
+      "https://item.kakaocdn.net/do/e0bb01952ddc96b36cb4975891e0ca8cf43ad912ad8dd55b04db6a64cddaf76d",
+  },
+
+  {
+    id: 5,
+    alt: "넹 앙꼬",
+    imgSrc:
+      "https://item.kakaocdn.net/do/c76a792c805d75c2f86d5564600a3d1bf43ad912ad8dd55b04db6a64cddaf76d",
+  },
+  {
+    id: 6,
+    alt: "띵가띵가 앙꼬",
+    imgSrc:
+      "https://item.kakaocdn.net/do/472cc51b79526f9747e43fc778d5c681f43ad912ad8dd55b04db6a64cddaf76d",
+  },
+  {
+    id: 7,
+    alt: "뇸뇸뇸 앙꼬",
+    imgSrc:
+      "https://item.kakaocdn.net/do/a773e7f1477c06654be7a0b878e6b7e7f43ad912ad8dd55b04db6a64cddaf76d",
+  },
+
+  {
+    id: 8,
+    alt: "스윗 앙꼬",
+    imgSrc:
+      "https://item.kakaocdn.net/do/791e6c0c6aa2f93fce7fda1b7fa0a545f43ad912ad8dd55b04db6a64cddaf76d",
+  },
+  {
+    id: 9,
+    alt: "뽜쟈 앙꼬",
+    imgSrc:
+      "https://item.kakaocdn.net/do/7295bec09972b22aa1c0aff77802235bf43ad912ad8dd55b04db6a64cddaf76d",
+  },
+];
+
+export default ANKO_DATA;
