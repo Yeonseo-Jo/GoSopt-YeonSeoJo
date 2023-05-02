44be533ad09c6602edd4876ee6e3c24be21eb717
Yeonseo-Jo <jys073190@gmail.com>
2023-05-02 16:56:16 +0900

    feat: main 섹션 ui

--- week3/card_game/src/App.jsx
+++ week3/card_game/src/App.jsx
@@ -1,4 +1,4 @@
-import { ThemeProvider } from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 import theme from "./styles/theme";
 import { GlobalStyle } from "./styles/GlobalStyle";
 import Header from "./components/Header";
@@ -7,9 +7,20 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Header />
+      <StContainer>
+        <Header />
+      </StContainer>
     </ThemeProvider>
   );
 }
+
+const StContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  background-color: ${({ theme }) => theme.colors.lightYellow};
+`;
 
 export default App;
