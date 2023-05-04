4c019561bf5e29b7ebe3a853173dc3e00aedb1bd
Yeonseo-Jo <jys073190@gmail.com>
2023-05-04 13:10:37 +0900

    fix: 레벨 버튼 css 수정

--- week3/card_game/src/components/LevelNav.jsx
+++ week3/card_game/src/components/LevelNav.jsx
@@ -36,8 +36,7 @@ const StLevelBtnContainer = styled.nav`
 `;
 
 const StLevelBtn = styled.button`
-  border: 0;
-  padding: 1.7rem;
+  padding: 1.5rem 1.8rem;
   margin-right: 2rem;
   background-color: ${({ selected, theme }) =>
     selected ? theme.colors.white : theme.colors.lightPink};
@@ -45,4 +44,5 @@ const StLevelBtn = styled.button`
   border-radius: 1rem;
   box-shadow: 0.3rem 0.3rem 0.3rem ${({ theme }) => theme.colors.lightPurple};
   font-size: 1.8rem;
+  font-weight: 700;
 `;
