6bf10e5a69c9cb4882e342287cbef5b150a3cd37
Yeonseo-Jo <jys073190@gmail.com>
2023-05-04 10:59:59 +0900

    refactor: Main -> Home 컴포넌트 명 변경

--- week3/card_game/src/App.jsx
+++ week3/card_game/src/App.jsx
@@ -1,13 +1,13 @@
 import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme";
 import { GlobalStyle } from "./styles/GlobalStyle";
-import Main from "./components/Main";
+import Home from "./components/Main";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Main />
+      <Home />
     </ThemeProvider>
   );
 }
