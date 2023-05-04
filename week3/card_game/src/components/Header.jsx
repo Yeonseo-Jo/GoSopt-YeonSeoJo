ea0c2c8c4eae36a8d5d2dd5b1eeeabe9b14158dd
Yeonseo-Jo <jys073190@gmail.com>
2023-05-04 10:58:37 +0900

    feat: 정답 시 글자 네온 애니메이션

--- week3/card_game/src/components/Header.jsx
+++ week3/card_game/src/components/Header.jsx
@@ -1,25 +1,23 @@
-// import { useState } from "react";
-import styled from "styled-components";
+import { useEffect, useRef } from "react";
+import styled, { keyframes } from "styled-components";
 
 export const Header = ({ currScore, totalScore }) => {
-  // let totalNum = 0;
-  // switch (currLevel) {
-  //   case "EASY":
-  //     totalNum = 5;
-  //     break;
-  //   case "NORMAL":
-  //     totalNum = 7;
-  //     break;
-  //   case "HARD":
-  //     totalNum = 9;
-  //     break;
-  // }
+  const scoreRef = useRef();
+
+  useEffect(() => {
+    if (currScore !== 0) {
+      scoreRef.current.classList.add("IsCorrect");
+      setTimeout(() => {
+        scoreRef.current.classList.remove("IsCorrect");
+      }, 1000);
+    }
+  }, [currScore]);
 
   return (
     <>
       <StHeaderContainer>
         <StTitle>💖 앙꼬를 맞춰주세요 💖</StTitle>
-        <StScore>
+        <StScore ref={scoreRef}>
           {currScore}/{totalScore}
         </StScore>
       </StHeaderContainer>
@@ -45,7 +43,23 @@ const StTitle = styled.h1`
   color: ${({ theme }) => theme.colors.purple};
 `;
 
+const neon = keyframes`
+  0%,
+  100% {
+    text-shadow: 0 0 1vw #FA1C16, 0 0 3vw #FA1C16, 0 0 10vw #FA1C16, 0 0 10vw #FA1C16, 0 0 .4vw #FED128, .5vw .5vw .1vw #806914;
+    color: #FED128;
+  }
+  50% {
+    text-shadow: 0 0 .5vw #800E0B, 0 0 1.5vw #800E0B, 0 0 5vw #800E0B, 0 0 5vw #800E0B, 0 0 .2vw #800E0B, .5vw .5vw .1vw #40340A;
+    color: #806914;
+  }
+`;
 const StScore = styled.h3`
   font-size: 5rem;
   color: ${({ theme }) => theme.colors.greyPurple};
+  &.IsCorrect {
+    animation: ${neon} 1s ease infinite;
+    -moz-animation: ${neon} 1s ease infinite;
+    -webkit-animation: ${neon} 1s ease infinite;
+  }
 `;
