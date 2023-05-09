ef5488e0c94fdd99552ba36fcec2dc73530b5005
Yeonseo-Jo <jys073190@gmail.com>
2023-05-10 02:17:08 +0900

    feat: header 컴포넌트 생성

---
+++ week4/forecast/src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { styled } from "styled-components";
+
+const Header = () => {
+  return (
+    <St.HeaderWrapper>
+      <h1> ☀️ 연또의 날씨 예보 ☀️ </h1>
+    </St.HeaderWrapper>
+  );
+};
+
+export default Header;
+
+const St = {
+  HeaderWrapper: styled.header`
+    display: flex;
+    width: 100%;
+    padding: 2rem;
+    background-color: ${({ theme }) => theme.colors.headerBlue};
+
+    & > h1 {
+      font-family: ${({ theme }) => theme.font.fontfamily};
+      font-style: normal;
+      font-weight: 700;
+      font-size: 5rem;
+      color: ${({ theme }) => theme.colors.white};
+    }
+  `,
+};
