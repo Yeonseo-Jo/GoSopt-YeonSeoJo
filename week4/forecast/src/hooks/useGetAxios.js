021d30f831e78462797b3a200c2efcc513bb8d08
seohee0112 <ghdtj7@naver.com>
2023-05-12 20:58:52 +0900

    fix : useReducer로 통신관리

--- week4/forecast/src/hooks/useGetAxios.js
+++ week4/forecast/src/hooks/useGetAxios.js
@@ -1,37 +1,76 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+import { useEffect } from "react";
+import { useReducer } from "react";
+
+// const useGetAxios = (url) => {
+//   // axios로 api 값을 get하는 custom hook
+
+//   // 받아온 데이터를 저장하는 state
+//   const [data, setData] = useState(null);
+//   // 로딩 중 플래그 state
+//   const [isLoading, setIsLoading] = useState(true);
+//   // 에러 저장 state.
+//   const [error, setError] = useState(null);
+
+//   // 데이터 받아오는 함수 try로 데이터를 받고, 없으면 catch문에서 error 넘겨준다.
+//   const fetchData = async () => {
+//     try {
+//       const response = await axios.get(url);
+//       setData(response.data);
+//     } catch (err) {
+//       setError(err.message);
+//     }
+//     setIsLoading(false);
+//   };
+
+//   // hook에 url이 들어올 때마다 실행한다
+//   useEffect(() => {
+//     setData(null);
+//     setIsLoading(true);
+//     setError("");
+//     fetchData();
+//   }, [url]);
+
+//   console.log(data);
+//   // axios로 get한 api data, 로딩 중 플래그, 에러를 반환한다.
+//   return { data, isLoading, error };
+// };
+
+// export default useGetAxios;
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "loding":
+      return { ...state, isLoding: true };
+    case "success":
+      return { isLoding: false, isError: false, data: action.data };
+    case "error":
+      return { isLoding: false, isError: true, data: action.error };
+  }
+};
 const useGetAxios = (url) => {
-  // axios로 api 값을 get하는 custom hook
+  const [state, dispatch] = useReducer(reducer, {
+    data: null,
+    isLoding: false,
+    isError: null,
+  });
+  console.log(url);
 
-  // 받아온 데이터를 저장하는 state
-  const [data, setData] = useState(null);
-  // 로딩 중 플래그 state
-  const [isLoading, setIsLoading] = useState(true);
-  // 에러 저장 state.
-  const [error, setError] = useState(null);
-
-  // 데이터 받아오는 함수 try로 데이터를 받고, 없으면 catch문에서 error 넘겨준다.
   const fetchData = async () => {
+    dispatch({ type: "loading" });
     try {
       const response = await axios.get(url);
-      setData(response.data);
+      dispatch({ type: "success", data: response.data });
     } catch (err) {
-      setError(err.message);
+      dispatch({ type: "error", data: err });
     }
-    setIsLoading(false);
   };
-
-  // hook에 url이 들어올 때마다 실행한다
   useEffect(() => {
-    setData(null);
-    setIsLoading(true);
-    setError("");
     fetchData();
   }, [url]);
-
-  // axios로 get한 api data, 로딩 중 플래그, 에러를 반환한다.
-  return { data, isLoading, error };
+  return state;
 };
 
 export default useGetAxios;
