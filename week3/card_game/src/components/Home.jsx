e59ee2980db2458f00c8894c610275c2e113dc9e
Yeonseo-Jo <jys073190@gmail.com>
2023-06-01 16:09:31 +0900

    fix : 랜덤 카드 배열 불러 오기 방식 변경(reset 됐을 때 & level 바뀌었을 때 새로고침 되도록)

--- week3/card_game/src/components/Home.jsx
+++ week3/card_game/src/components/Home.jsx
@@ -9,13 +9,29 @@ import ResetBtn from "./ResetBtn";
 import SuccessModal from "./SuccessModal";
 
 // 랜덤 처리한 카드 이미지 데이터 호출
-import {
-  EasyRandomList,
-  HardRandomList,
-  NormalRandomList,
-} from "../utils/shuffleData";
+import { randomCardList, shuffle } from "../utils/shuffleData";
 
 export const Home = () => {
+  // 레벨에 따른 카드 리스트 상수 정의 (util 파일이 아닌 여기서 호출해야 useEffect 처리 가능)
+  const EASY_RANDOM_LIST = shuffle([
+    ...randomCardList.slice(0, 5),
+    ...randomCardList.slice(0, 5),
+  ]);
+
+  const NORMAL_RANDOM_LIST = shuffle([
+    ...randomCardList.slice(0, 7),
+    ...randomCardList.slice(0, 7),
+  ]);
+
+  const HARD_RANDOM_LIST = shuffle([
+    ...randomCardList.slice(0, 9),
+    ...randomCardList.slice(0, 9),
+  ]);
+  // 레벨에 따른 total score 상수 정의
+  const EASY_SCORE = 5;
+  const NORMAL_SCORE = 7;
+  const HARD_SCORE = 9;
+
   // 선택 된 레벨을 저장하는 state, default 값은 EASY 레벨로 지정
   const [currLevel, setCurrLevel] = useState("EASY");
   // 현재 점수를 저장하는 state
@@ -24,28 +40,32 @@ export const Home = () => {
   const [isReset, setIsReset] = useState(false);
   // 게임이 끝나 성공 축하 모달이 열렸는지 여부를 저장하는 state
   const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const EASY_SCORE = 5;
-  const NORMAL_SCORE = 7;
-  const HARD_SCORE = 9;
-
   // 선택된 레벨에 따라 카드 리스트(개수 다르게)를 shuffleData에서 불러오고, totalScore도 다르게 지정
-  let currCardList;
-  let totalScore;
-  switch (currLevel) {
-    case "EASY":
-      currCardList = EasyRandomList;
-      totalScore = EASY_SCORE;
-      break;
-    case "NORMAL":
-      currCardList = NormalRandomList;
-      totalScore = NORMAL_SCORE;
-      break;
-    case "HARD":
-      currCardList = HardRandomList;
-      totalScore = HARD_SCORE;
-      break;
-  }
+  const [currCardList, setCurrCardList] = useState(EASY_RANDOM_LIST);
+  const [totalScore, setTotalScore] = useState(EASY_SCORE);
+
+  const shuffleCardList = () => {
+    switch (currLevel) {
+      case "EASY":
+        setCurrCardList(EASY_RANDOM_LIST);
+        setTotalScore(EASY_SCORE);
+        break;
+      case "NORMAL":
+        setCurrCardList(NORMAL_RANDOM_LIST);
+        setTotalScore(NORMAL_SCORE);
+        break;
+      case "HARD":
+        setCurrCardList(HARD_RANDOM_LIST);
+        setTotalScore(HARD_SCORE);
+        break;
+    }
+  };
+
+  console.log(currCardList);
+
+  useEffect(() => {
+    shuffleCardList();
+  }, [currLevel, isReset]);
 
   // 점수가 만점이 되면 모달을 불러오기 위한 useEffect
   useEffect(() => {
