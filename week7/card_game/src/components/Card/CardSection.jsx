5086dabf876009b9f5c34fa9db8c6b370a632feb
Yeonseo-Jo <jys073190@gmail.com>
2023-06-02 15:42:54 +0900

    init: 3주차 과제 폴더 복사

---
+++ week7/card_game/src/components/Card/CardSection.jsx
@@ -0,0 +1,93 @@
+import styled from "styled-components";
+import { useEffect, useState } from "react";
+
+import Card from "./Card";
+
+export const CardSection = ({
+  currLevel,
+  currCardList,
+  setCurrScore,
+  isReset,
+  setIsReset,
+}) => {
+  // 클릭 된 카드의 idx를 저장하는 state
+  const [clickedList, setClickedList] = useState([]);
+  // 클릭 된 카드의 카드 정보를 저장하는 state
+  const [matchedList, setmatchedList] = useState([]);
+
+  // 카드 클릭 시 로직 처리를 위한 useEffect
+  useEffect(() => {
+    // 카드가 2개 선택되고, 그 두 카드 정보가 일치하면
+    if (matchedList.length === 2) {
+      if (matchedList[0] === matchedList[1]) {
+        //카드 데이터의 matchedStatus를 true로 바꿔주고, score를 올려준다.
+        matchedList[0].matchedStatus = true;
+        setCurrScore((prev) => prev + 1);
+      }
+      // 일치하지 않으면 0.7초 뒤에 뒤집기
+      setTimeout(() => {
+        setmatchedList([]);
+        setClickedList([]);
+      }, 700);
+    }
+  }, [matchedList]);
+
+  // 레벨이 바뀌거나, 리셋되면 초기화하기 위한 useEffect
+  useEffect(() => {
+    setmatchedList([]);
+    setClickedList([]);
+    setCurrScore(0);
+    currCardList.forEach((card) => {
+      card.matchedStatus = false;
+    });
+    setIsReset(false);
+  }, [currLevel, isReset]);
+
+  // 카드 선택 시 관련 state를 바꿔주기 위한 함수, card 컴포넌트의 click 이벤트로 처리할 수 있게 넘겨준다
+  const handleCardChoice = (card, idx) => {
+    setmatchedList([...matchedList, card]);
+    setClickedList([...clickedList, idx]);
+  };
+
+  return (
+    <StCardsContainer>
+      {currCardList.map((card, idx) => {
+        return (
+          <>
+            <StCardWrapper
+              key={`${card.id}-${idx}`}
+              id={idx}
+              clasName={card.id}
+            >
+              <Card
+                card={card}
+                idx={idx}
+                handleCardChoice={
+                  clickedList.length < 2 ? handleCardChoice : null
+                }
+                isFlipped={clickedList.includes(idx) || card.matchedStatus}
+              />
+            </StCardWrapper>
+          </>
+        );
+      })}
+    </StCardsContainer>
+  );
+};
+
+export default CardSection;
+
+const StCardsContainer = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  padding: 0 8rem;
+  background-color: ${({ theme }) => theme.colors.lightYellow};
+`;
+
+const StCardWrapper = styled.article`
+  margin: 1rem;
+  width: 20rem;
+  height: 25rem;
+  perspective: 100rem;
+`;
---
+++ week7/card_game/src/components/LevelNav.jsx
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+export const LevelNav = ({ currLevel, setCurrLevel }) => {
+  // 선택할 수 있는 레벨 리스트
+  const levelList = ["EASY", "NORMAL", "HARD"];
+
+  return (
+    <>
+      <StLevelBtnContainer>
+        {levelList.map((level) => {
+          return (
+            <StLevelBtn
+              key={level}
+              id={level}
+              type="button"
+              onClick={() => {
+                setCurrLevel(level);
+              }}
+              selected={level === currLevel ? true : false}
+            >
+              {level}
+            </StLevelBtn>
+          );
+        })}
+      </StLevelBtnContainer>
+    </>
+  );
+};
+
+export default LevelNav;
+
+const StLevelBtnContainer = styled.nav`
+  display: flex;
+  justify-content: center;
+  padding: 1.5rem;
+`;
+
+const StLevelBtn = styled.button`
+  padding: 1.5rem 1.8rem;
+  margin-right: 2rem;
+  background-color: ${({ selected, theme }) =>
+    selected ? theme.colors.white : theme.colors.lightPink};
+  border: none;
+  border-radius: 1rem;
+  box-shadow: 0.3rem 0.3rem 0.3rem ${({ theme }) => theme.colors.lightPurple};
+  font-size: 1.8rem;
+  font-weight: 700;
+`;
