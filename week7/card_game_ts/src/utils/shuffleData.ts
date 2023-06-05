5cbf9cf469ba1022a1e22c1c773f95d581fd302b
Yeonseo-Jo <jys073190@gmail.com>
2023-06-05 16:49:14 +0900

    feat : ts migration - shuffleData util의 shuffle 함수 매개변수 type 지정

--- week7/card_game_ts/src/utils/shuffleData.ts
+++ week7/card_game_ts/src/utils/shuffleData.ts
@@ -1,7 +1,8 @@
 import ANKO_DATA from "../assets/datas/ANKO_DATA";
+import { CardObject } from "../types/card";
 
 // 데이터 랜덤 정렬 함수
-export const shuffle = (array) => {
+export const shuffle = (array: Array<CardObject>) => {
   let shuffleArray = array.sort(() => Math.random() - 0.5);
   return shuffleArray;
 };
