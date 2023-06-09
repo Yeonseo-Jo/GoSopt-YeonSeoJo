3a7c8f4baa2ed837d6c5111df5311ba58c968488
Yeonseo-Jo <jys073190@gmail.com>
2023-06-09 21:11:05 +0900

    fix : selector type error 수정 git add .! newValue에 type 정해주기

--- week7/card_game_ts/src/recoil/selector.ts
+++ week7/card_game_ts/src/recoil/selector.ts
@@ -1,28 +1,33 @@
 import { selector } from "recoil";
 
-import { GameState } from "../types/game";
 import { gameStateAtom } from "./atom";
 
-export const currLevelSelector = selector<GameState>({
+export const currLevelSelector = selector<string>({
   key: "currLevelSelector",
   get: ({ get }) => get(gameStateAtom).currLevel,
-  set: ({ set, get }, newValue) => {
-    set(gameStateAtom, { ...get(gameStateAtom), currLevel: newValue });
+  set: ({ set }, newValue) => {
+    set(gameStateAtom, (prev) => ({
+      ...prev,
+      currLevel: newValue as string,
+    }));
   },
 });
 
-export const currScoreSelector = selector({
+export const currScoreSelector = selector<number>({
   key: "currScoreSelector",
   get: ({ get }) => get(gameStateAtom).currScore,
-  set: ({ set, get }, newValue) => {
-    set(gameStateAtom, { ...get(gameStateAtom), currScore: newValue });
+  set: ({ set }, newValue) => {
+    set(gameStateAtom, (prev) => ({
+      ...prev,
+      currScore: newValue as number,
+    }));
   },
 });
 
-export const totalScoreSelector = selector({
+export const totalScoreSelector = selector<number>({
   key: "totalScoreSelector",
   get: ({ get }) => get(gameStateAtom).totalScore,
-  set: ({ set, get }, newValue) => {
-    set(gameStateAtom, { ...get(gameStateAtom), totalScore: newValue });
+  set: ({ set }, newValue) => {
+    set(gameStateAtom, (prev) => ({ ...prev, totalScore: newValue as number }));
   },
 });
